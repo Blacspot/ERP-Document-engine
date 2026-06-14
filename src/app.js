687653cf8df82ca32bc84e2e090524fb3e39d76d
mr blacspot <obwogemcbride73@gmail.com>
2026-06-14 19:23:13 +0300

    Engine initialization

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Document Engine is running"
+    });
+});
+export default app;
